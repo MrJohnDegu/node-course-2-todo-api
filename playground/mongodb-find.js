9f6d3d24f5637f5a0fcebf317d909b5a3cc22a24
JC De Guzman <JC De Guzman>
2017-05-01 04:31:57 +0800

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server', err);
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID("59063ee74680444d53cf7a56")
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+
+    // db.collection('Todos').findOne({completed: true}, (err, res) => {
+    //     console.log(err);
+    //     console.log(res);
+    // });
+
+
+    db.collection('Users').find({name: 'John Carlo De Guzman'}).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    // db.close();
+});
