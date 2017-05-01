356d96871312036d95aeaf3525d46f3be115f3ce
JC De Guzman <JC De Guzman>
2017-05-01 21:21:07 +0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -30,25 +30,4 @@ app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
-
-// var otherTodo = new Todo({
-//     text: 'Cook dinner',
-//     completed: true,
-//     completedAt: 200,
-// });
-
-// otherTodo.save().then((res) => {
-//     console.log('Saved todo', res);
-// }, (err) => {
-//     console.log('Unable to save todo');
-// });
-
-// var newUser = new User({
-//     email:'    jcdeguzman88@gmail.com   ',
-// });
-
-// newUser.save().then((res) => {
-//     console.log('Saved user', res);
-// }, (err) => {
-//     console.log('Unable to save user');
-// });
+module.exports = { app };
