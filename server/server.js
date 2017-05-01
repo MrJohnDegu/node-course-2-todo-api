fdccd6ba18a22ef7578d0fa09c0f8ec20a2b26c8
JC De Guzman <JC De Guzman>
2017-05-01 23:24:01 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -25,9 +26,29 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
-    }, (err) => {   
+    }, (e) => {   
         res.status('400').send(e);
     })    
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        res.status('404').send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status('404').send();
+        }
+
+        res.send({todo});
+        
+    }, (e) => {
+        res.status('400').send();
+    })
 });
 
 app.listen(port, () => {
